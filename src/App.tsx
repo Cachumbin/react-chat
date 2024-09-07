f8ad54844fb5853e62540da6bb1c3c8242386ccd
Cachumbin <simoncarreno2006@gmail.com>
2024-09-06 23:33:10 -0500

    feat: Initialized the firebase sdk (not configured yet)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
 import "./App.css"
 
+import firebase from "firebase/app"
+import "firebase/firestore"
+import "firebase/auth"
+
+import { useAuthState } from "react-firebase-hooks/auth"
+import { useCollectionData } from "react-firebase-hooks/firestore"
+
+firebase.initializeApp({
+  // Your firebase config
+})
+
+const auth = firebase.auth()
+const firestore = firebase.firestore()
+
 const App = () => {
   return (
     <div>App</div>
