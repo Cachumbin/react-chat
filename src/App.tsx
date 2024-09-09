1baa35062ac91e48860b98693a023568ceac4ca5
Cachumbin <simoncarreno2006@gmail.com>
2024-09-09 18:02:15 -0500

    fix: Mayor changes to make app work
    That auth and firestore were changed to the v9+ firebase version hooks, the imports were updated, typescript firebase types and component types were added

--- src/App.tsx
+++ src/App.tsx
@@ -1,85 +1,81 @@
-import "./App.css"
+import "./App.css";
+import { initializeApp } from "firebase/app";
+import {
+  getFirestore,
+  collection,
+  query,
+  orderBy,
+  limit,
+} from "firebase/firestore";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
 
-import firebase from "firebase/app"
-import { initializeApp } from "firebase/app"
-import { getFirestore } from "firebase/firestore"
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth"
-import "firebase/firestore"
-import "firebase/auth"
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
+};
 
-import { useAuthState } from "react-firebase-hooks/auth"
-import { useCollectionData } from "react-firebase-hooks/firestore"
-import dotenv from "dotenv"
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const firestore = getFirestore(app);
 
-dotenv.config()
+const SignIn = () => {
+  const signInWithGoogle = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider);
+  };
 
-firebase.initializeApp({
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  projectId: process.env.projectId,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId,
-  measurementId: process.env.measurementId,
-})
-
-const auth = firebase.auth()
-const firestore = firebase.firestore()
-
-const SignIn = ({}) => {
-  const useSignInWithGoogle = () => {
-      const provider = new firebase.auth.GoogleAuthProvider()
-      auth.signInWithPopup(provider)
-  }
-
-return (
-  <button onClick={useSignInWithGoogle}>Sign in with Google</button>
-)
-}
+  return <button onClick={signInWithGoogle}>Sign in with Google</button>;
+};
 
 const SignOut = () => {
-  return auth.currentUser && (
-    <button onClick={() => auth.signOut()}>Sign Out</button>
-  )
-}
+  return (
+    auth.currentUser && <button onClick={() => signOut(auth)}>Sign Out</button>
+  );
+};
 
 const ChatRoom = () => {
-  const messagesRef = firestore.collection("messages")
-  const query = messagesRef.orderBy("createdAt").limit(25)
+  const messagesRef = collection(firestore, "messages");
+  const q = query(messagesRef, orderBy("createdAt"), limit(25));
 
-  const [messages] = useCollectionData(query, { idField: "id" } as any)
-
-  return (
-    <>
-      <div>
-        {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
-      </div>
-    </>
-  )
-}
-
-const ChatMessage = ({ message }) => {
-  const { text, uid } = message
+  const [messages] = useCollectionData(q);
 
   return (
-    <p>{text}</p>
-  )
-}
+    <div>
+      {messages &&
+        messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
+    </div>
+  );
+};
+
+const ChatMessage = ({ message }: { message: any }) => {
+  const { text } = message;
+
+  return <p>{text}</p>;
+};
 
 const App = () => {
-
-  const [user] = useAuthState(auth)
+  const [user] = useAuthState(auth);
 
   return (
     <div>
       <header>
-
+        <SignOut />
       </header>
-      <section>
-        {user ? <ChatRoom /> : <SignIn />}
-      </section>
+      <section>{user ? <ChatRoom /> : <SignIn />}</section>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
