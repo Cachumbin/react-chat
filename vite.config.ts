5409133c8ddfde728ce468a4d3bb5fec7a5d3c1d
Cachumbin <simoncarreno2006@gmail.com>
2024-09-12 17:50:38 -0500

    fix: Fixed declaration of env variables

--- vite.config.ts
+++ vite.config.ts
@@ -8,13 +8,13 @@ export default defineConfig({
   plugins: [react()],
   define: {
     'process.env': {
-      REACT_APP_API_KEY: process.env.REACT_APP_API_KEY,
-      REACT_APP_AUTH_DOMAIN: process.env.REACT_APP_AUTH_DOMAIN,
-      REACT_APP_PROJECT_ID: process.env.REACT_APP_PROJECT_ID,
-      REACT_APP_STORAGE_BUCKET: process.env.REACT_APP_STORAGE_BUCKET,
-      REACT_APP_MESSAGING_SENDER_ID: process.env.REACT_APP_MESSAGING_SENDER_ID,
-      REACT_APP_APP_ID: process.env.REACT_APP_APP_ID,
-      REACT_APP_MEASUREMENT_ID: process.env.REACT_APP_MEASUREMENT_ID,
+      VITE_API_KEY: process.env.VITE_API_KEY,
+      VITE_AUTH_DOMAIN: process.env.VITE_AUTH_DOMAIN,
+      VITE_PROJECT_ID: process.env.VITE_PROJECT_ID,
+      VITE_STORAGE_BUCKET: process.env.VITE_STORAGE_BUCKET,
+      VITE_MESSAGING_SENDER_ID: process.env.VITE_MESSAGING_SENDER_ID,
+      VITE_APP_ID: process.env.VITE_APP_ID,
+      VITE_MEASUREMENT_ID: process.env.VITE_MEASUREMENT_ID,
     },
   },
 });
