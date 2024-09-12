0f696df17e5f952431dba34c79aa485154b8b4c5
Cachumbin <simoncarreno2006@gmail.com>
2024-09-12 13:00:34 -0500

    build: Installed Tailwind CSS

--- src/Components/firebaseConfig.tsx
+++ src/Components/firebaseConfig.tsx
@@ -4,12 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "your-api-key",
-  authDomain: "your-project-id.firebaseapp.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project-id.appspot.com",
-  messagingSenderId: "your-messaging-sender-id",
-  appId: "your-app-id",
+  apiKey: "a",
+  authDomain: "a",
+  projectId: "a",
+  storageBucket: "a",
+  messagingSenderId: "a",
+  appId: "a",
+  measurementId: "a",
 };
 
 const app = initializeApp(firebaseConfig);
