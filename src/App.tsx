fcb92d56c4b4819e7e64a75ff3d9408fb13138bd
Cachumbin <simoncarreno2006@gmail.com>
2024-09-13 08:43:08 -0500

    style: Centered the chat and changed the tab title

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "./Components/firebaseConfig";
 import ChatRoom from "./Components/ChatRoom";
@@ -9,14 +8,16 @@ const App = () => {
   const [user] = useAuthState(auth);
 
   return (
-    <div className="max-w-2xl flex flex-col">
-      <header className="bg-gradient-to-r from-pink-500 to-yellow-500 flex justify-between">
-        <p className="m-6 text-2xl font-bold text-white">CachunChat</p>
-        <SignOut />
-      </header>
-      <section className="bg-gradient-to-br from-pink-100 to-yellow-100">
-        {user ? <ChatRoom /> : <SignIn />}
-      </section>
+    <div className="flex justify-center">
+      <div className="m-8 max-w-2xl flex flex-col">
+        <header className="bg-gradient-to-r from-pink-500 to-yellow-500 flex justify-between">
+          <p className="m-6 text-2xl font-bold text-white">CachunChat</p>
+          <SignOut />
+        </header>
+        <section className="bg-gradient-to-br from-pink-100 to-yellow-100">
+          {user ? <ChatRoom /> : <SignIn />}
+        </section>
+      </div>
     </div>
   );
 };
