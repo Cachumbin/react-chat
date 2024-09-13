d687741599f1a22af27dd388c6222f759ec094f1
Cachumbin <simoncarreno2006@gmail.com>
2024-09-13 09:19:31 -0500

    feat: Recreated the file span to show the current file

--- src/Components/ChatRoom.tsx
+++ src/Components/ChatRoom.tsx
@@ -111,26 +111,33 @@ const ChatRoom: React.FC = () => {
           ))}
       </div>
 
-      <form className="w-2-xl flex bg-white p-2" onSubmit={sendMessage}>
-        <input
-          className="w-[80%] bg-gray-100 m-2 rounded-xl border-2 border-gray-300"
-          value={formValue}
-          onChange={(e) => setFormValue(e.target.value)}
-          placeholder="Type your message"
-        />
-        <label htmlFor="file-upload" className="cursor-pointer my-4">
-          <GrAttachment className="text-2xl mx-2 bg-pink-500 w-12 h-12 rounded-md p-2 text-white hover:text-black hover:bg-yellow-300 transition duration-300 ease-in-out" />
-        </label>
-        <input
-          id="file-upload"
-          className="hidden"
-          type="file"
-          onChange={(e) => setFile(e.target.files ? e.target.files[0] : null)}
-        />
-        <button type="submit" disabled={!formValue && !file}>
-          <TbSend2 className="text-2xl mx-2 bg-pink-500 w-12 h-12 rounded-md p-2 text-white hover:text-black hover:bg-yellow-300 transition duration-300 ease-in-out" />
-        </button>
-      </form>
+      <div className="flex flex-col">
+        {file && (
+          <span className="bg-white text-sm text-gray-600 p-2">
+            {file.name}
+          </span>
+        )}
+        <form className="w-2-xl flex bg-white p-2" onSubmit={sendMessage}>
+          <input
+            className="w-[80%] bg-gray-100 m-2 rounded-xl border-2 border-gray-300"
+            value={formValue}
+            onChange={(e) => setFormValue(e.target.value)}
+            placeholder="Type your message"
+          />
+          <label htmlFor="file-upload" className="cursor-pointer my-4">
+            <GrAttachment className="text-2xl mx-2 bg-pink-500 w-12 h-12 rounded-md p-2 text-white hover:text-black hover:bg-yellow-300 transition duration-300 ease-in-out" />
+          </label>
+          <input
+            id="file-upload"
+            className="hidden"
+            type="file"
+            onChange={(e) => setFile(e.target.files ? e.target.files[0] : null)}
+          />
+          <button type="submit" disabled={!formValue && !file}>
+            <TbSend2 className="text-2xl mx-2 bg-pink-500 w-12 h-12 rounded-md p-2 text-white hover:text-black hover:bg-yellow-300 transition duration-300 ease-in-out" />
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
