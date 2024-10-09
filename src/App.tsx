42d90670bd5fff14b5e915b4ba9c507093624efc
Cachumbin <simoncarreno2006@gmail.com>
2024-10-09 17:23:22 -0500

    fix: (#2) Fixed the height and more styles
    The styles are completed in mobile

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,11 @@ const App = () => {
   const [user] = useAuthState(auth);
 
   return (
-    <div className="flex justify-center h-[850px] overflow-hidden">
+    <div className="flex justify-center h-[700px] sm:h-[850px] overflow-hidden ">
       <div className="mx-2 sm:mx-8 max-w-2xl flex flex-col h-full overflow-hidden">
-        <header className="bg-gradient-to-r from-pink-500 to-yellow-500 flex justify-between">
+        <header className="bg-gradient-to-r from-pink-500 to-yellow-500 flex justify-between items-center">
           <img src="/public/Recurso 1.png" alt="" className="w-20 h-20 p-2" />
-          <p className="m-6 text-2xl font-bold text-white">CachunChat</p>
+          <p className="m-4 text-2xl font-bold text-white">CachunChat</p>
           <SignOut />
         </header>
         <section className="bg-gradient-to-br from-pink-100 h-[800px] to-yellow-100 flex flex-col flex-grow overflow-hidden">
